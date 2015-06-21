1a8f6623391ae9b3679fd9dd31a0160c3843ae13
Jeshua Maxey <me@jeshua.co>
2015-06-21 14:35:25 +0100

    add nav styles for small displays

--- client/templates/application/application.js
+++ client/templates/application/application.js
@@ -4,9 +4,7 @@ Template.application.rendered = function  () {
   // hide with js so if the user doesn't have js installed they
   // can still use the nav
   $('#navigation').hide();
-  console.debug($('#navigation'))
   $('#toggleNav').on('click', function() {
-    console.debug('poop')
     $('#navigation').slideToggle();
   });
 }
