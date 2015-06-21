ad5d54aaa34aaaf867941700f7891f8a1da14437
Jeshua Maxey <me@jeshua.co>
2015-06-21 14:43:31 +0100

    add js to handle update in viewport width. Nav works on large screens now

--- client/templates/application/application.js
+++ client/templates/application/application.js
@@ -3,8 +3,22 @@ Template.application.rendered = function  () {
   /* nav javascript */
   // hide with js so if the user doesn't have js installed they
   // can still use the nav
-  $('#navigation').hide();
+  if($(window).width() < 780) {
+      $('#navigation').hide();
+    }
   $('#toggleNav').on('click', function() {
     $('#navigation').slideToggle();
   });
+
+  $(window).on('resize', function() {
+    // make sure nav isn't hidden when viewport width is increased beyond
+    // breakpoint
+    if($(window).width() > 780) {
+      $('#navigation').show();
+    }
+    // collapse nav when the viewport width is decreased below breakpoint
+    if($(window).width() < 780) {
+      $('#navigation').hide();
+    }
+  })
 }
