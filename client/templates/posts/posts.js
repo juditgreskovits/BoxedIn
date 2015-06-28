dd2e6b28e5345dbcc133f69c437f0ae970a77b0e
Jeshua Maxey <me@jeshua.co>
2015-06-28 13:50:40 +0100

    Animated questions:
    Add flip animation to update the header question every 5secs

--- client/templates/posts/posts.js
+++ client/templates/posts/posts.js
@@ -1,3 +1,7 @@
+Session.setDefault('front', true);
+Session.setDefault('flipDuration', 800);
+Session.setDefault('colorOffset', 0);
+
 Template.posts.helpers({
 
 	posts: function() {
@@ -13,6 +17,50 @@ Template.postsPost.events({
 		}
 	}
 });
+
+function randomQuestion() {
+  var titles = Title.find().fetch();
+  var word0 = titles[getRandomIndex(titles.length)];
+  var word1 = word0.after[getRandomIndex(word0.after.length)];
+  var word2 = word1.after[getRandomIndex(word1.after.length)];
+  var word3 = {title: 'gender-q'};
+  return question = [word0.title, word1.title, word2.title, word3.title];
+}
+
+function refreshQuestion() {
+  var question = randomQuestion();
+  var url = '';
+  var colourClass = '';
+  var num;
+  var selector = Session.get('front') ? '.word .front' : '.word .back';
+
+  $(selector).each(function(i, word) {
+    // update bg image (the word)
+    url = 'img/words/'+ i +'/' + question[i] + '.png';
+    $(word).css('background-image', 'url('+ url +')');
+    $(word).css('background-position', '50% 50%');
+    $(word).css('transition-delay', i*0.3+'s');
+    $(word).css('-webkit-transition-delay', i*0.3+'s');
+    // update bg colour
+    $(word).removeClass(function (index, css) {
+      return (css.match (/(^|\s)colour-\S+/g).join(' '));
+    });
+    num = (i + Session.get('colorOffset'))%4;
+    colourClass = 'colour-' + num;
+    $(word).addClass(colourClass);
+  });
+
+  // the flip.jquery forces all transitions on the flip cards to be flipDuration
+  // this means that when we update the bg images, we have to wait that long
+  // for them to fade out/in. We hide this by not flipping the cards until
+  // flipDuration(ms) after we update the bg images
+  setTimeout(function() {
+    $('.word').flip(Session.get('front'))
+  }, Session.get('flipDuration'));
+
+  Session.set('front', !Session.get('front'));
+  Session.set('colorOffset', Session.get('colorOffset')+1)
+}
 
 Template.posts.rendered = function() {
   $grid = $('#posts-grid');
@@ -34,21 +82,24 @@ Template.posts.rendered = function() {
     height: 200
   });
 
-  //
-  var titles = Title.find().fetch();
-  var word0 = titles[getRandomIndex(titles.length)];
-  var word1 = word0.after[getRandomIndex(word0.after.length)];
-  var word2 = word1.after[getRandomIndex(word1.after.length)];
-  var word3 = {title: 'gender-q'};
-  var question = [word0.title, word1.title, word2.title, word3.title];
-
-  console.log(question);
-  var url = '';
-
-  $('.word').each(function(i, word) {
-    url = 'img/words/'+ i +'/' + question[i] + '.png';
-    $(word).css('background-image', 'url('+ url +')');
-    $(word).css('background-position', '50% 50%');
+  // initialise questions
+  $('.word').flip({
+    trigger: 'manual',
+    axis: 'x',
+    speed: Session.get('flipDuration')
   });
+  refreshQuestion();
+
+  // set them going
+  setInterval(function() {
+    refreshQuestion();
+  }, 5*1000);
+
+  // // debugging - press f key to flip the question
+  // $(document).keypress(function(e) {
+  //   if(e.which === 102) {
+  //     refreshQuestion();
+  //   }
+  // })
 
 }
