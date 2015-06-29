b8a430c97476cf327cf1fc67c160f7d2341d53a0
Jeshua Maxey <me@jeshua.co>
2015-06-29 22:33:39 +0100

    Word sizing:
    Fix word sizing bugs for small screen sizes

--- client/templates/posts/posts.js
+++ client/templates/posts/posts.js
@@ -41,6 +41,7 @@ function refreshQuestion() {
   var colourClass = '';
   var num;
   var selector = Session.get('front') ? '.word .front' : '.word .back';
+  var width = $(window).width();
 
   $(selector).each(function(i, word) {
     // update bg image (the word)
@@ -49,6 +50,24 @@ function refreshQuestion() {
     $(word).css('background-position', '50% 50%');
     $(word).css('transition-delay', i*0.3+'s');
     $(word).css('-webkit-transition-delay', i*0.3+'s');
+    // for longer words we have to shrink it to fit on small screens
+    // 405 chosen by trusty trial and error
+    if(width < 405) {
+      if(question[i].length > 8) {
+        $(word).css('background-size', '140%');
+      } else if(question[i].length > 6) {
+        $(word).css('background-size', '170%');
+      } else {
+        $(word).css('background-size', 'auto');
+      }
+      // don't apply this to the last card though, it doesn't need it
+      if(i === 3) {
+        $(word).css('background-size', 'auto');
+      }
+    } else {
+      // all sized auto for desktop
+      $(word).css('background-size', 'auto');
+    }
     // update bg colour
     $(word).removeClass(function (index, css) {
       return (css.match (/(^|\s)colour-\S+/g).join(' '));
@@ -95,12 +114,20 @@ Template.posts.rendered = function() {
   refreshQuestion();
 
   // set them going
-  var interval = setInterval(function() {
-    refreshQuestion();
-  }, 5*1000);
+  // var interval = setInterval(function() {
+  //   refreshQuestion();
+  // }, 5*1000);
   // store this in the session so we can cancel it when the user
   // navigates to another route
-  Session.set('interval', interval);
+  // Session.set('interval', interval);
+
+  //debug
+  $(window).keypress(function(event) {
+    if(event.which === 102) {
+      refreshQuestion();
+    }
+  })
+  //end debug
 
   // re
   $(window).resize(initQuestionAnimation)
