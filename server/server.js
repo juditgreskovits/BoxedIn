1973ba23e4a17dfd4ce780eb28f339076a52a240
Judit Greskovits <juditgreskovits@gmail.com>
2015-07-16 22:47:00 +0100

    Delete server.js

--- server/server.js
+++
@@ -1,8 +0,0 @@
-Accounts.validateLoginAttempt(function(info) {
-	var user = info.user;
-	var googleServices = user.services.google;
-	if(googleServices.email === 'boxedinproject@gmail.com' || googleServices.email === 'hellomelissabooth@gmail.com' || googleServices.email === 'hellojamesbooth@gmail.com' || googleServices.email === 'juditgreskovits@gmail.com' || googleServices.email === 'me@jeshua.co') {
-		return true;
-	}
-	return false;
-});
